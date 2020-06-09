d99c62e523fefed9423a93977d396e501eebe9ed
Ishaan Jain <ishaan160799@gmail.com>
2020-06-09 19:56:03 +0530

    val complete

--- src/First.kt
+++ src/First.kt
@@ -1,4 +1,12 @@
-fun main(){
-    val a = "Hello World from variable 'a'"
+fun main() {
+    val a: String = "Hello World from variable 'a'"
+    val b: Int = 10
+    val c: Boolean = false
+    val d: Float = 3.14F
+    val e: Double = 3.145798
     println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
 }
